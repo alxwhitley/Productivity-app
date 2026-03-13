c2e6642a477fb773f2eedded7f4977df32ffd2d6
Alex Whitley <alexwhitley@Alexs-MBP-7.lan>
2026-03-13 13:46:57 -0400

    clean up repo structure

--- nave-source_1/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://fezgmuhrgbtzlworbsep.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZlemdtdWhyZ2J0emx3b3Jic2VwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNjgyNTEsImV4cCI6MjA4ODc0NDI1MX0.YxHafnW1m287XRDiiRWTIPF2rKA1xEsQ4ArCeJO0ul8'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
